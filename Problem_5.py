a361b3bd823f5ad1d0db925a4791d0995c796228
Max <maxtrivedi@icloud.com>
2020-03-05 10:16:10 +0000

    Create Problem_5.py

---
+++ Problem_5.py
@@ -0,0 +1,7 @@
+a = 0
+complete = False
+while complete == False:
+    a += 20
+    if a % 1 == 0 and a % 2 == 0 and a % 3 == 0 and a % 4 == 0 and a % 5 == 0 and a % 6 ==0 and a % 7 == 0 and a % 8 ==0 and a % 9 == 0 and a % 10 ==0 and a % 11 == 0 and a % 12 ==0 and a % 13 == 0 and a % 14 ==0 and a % 15 == 0 and a % 16 ==0 and a % 17 == 0 and a % 18 ==0 and a % 19 == 0 and a % 20 == 0:
+        print(a)
+        complete = True
